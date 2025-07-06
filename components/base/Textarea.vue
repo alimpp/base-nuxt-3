0a3346112cd4a18a1cb2f6c3f0e0352a8fc1923c
alimpp <hmdmpp@gmail.com>
2025-07-06 12:13:48 -0700

    Update textarea

--- components/base/Textarea.vue
+++ components/base/Textarea.vue
@@ -1,85 +1,155 @@
 <template>
-    <div class="flex flex-column">
-      <span class="f-s-12 f-w-500" :class="{ 'color-danger': errorMessage }">
-        {{ label }}
-      </span>
-      <textarea
-        rows="10"
-        cols="30"
-        :style="{
-          width: `${width}`,
-        }"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        :type="type"
-        :class="{
-          error: errorMessage,
-        }"
-        :disabled="disabled"
-        :placeholder="placeholder"
-        class="mt-2"
-      ></textarea>
-      <span v-if="errorMessage" class="color-danger f-s-12 f-w-500">
-        {{ errorMessage }}
-      </span>
-    </div>
-  </template>
-  
-  <script setup>
-  const props = defineProps({
-    disabled: {
-      type: Boolean,
-      default: false,
-      required: false,
-    },
-    label: {
-      type: String,
-      default: "",
-      required: false,
-    },
-    type: {
-      type: String,
-      default: "",
-      required: false,
-    },
-    errorMessage: {
-      type: String,
-      default: "",
-      required: false,
-    },
-    modelValue: {
-      type: String,
-      default: "",
-      required: false,
-    },
-    placeholder: {
-      type: String,
-      default: "",
-      required: false,
-    },
-    height: {
-      type: String,
-      default: "40px",
-    },
-    width: {
-      type: String,
-      default: "",
-    },
-  });
-  </script>
-  
-  <style scoped lang="scss">
-  textarea {
-    padding: 10px 7px;
-    outline: none;
-    border: 1px solid #8481812f;
-    border-radius: 6px;
-    color: black;
-  }
-  
-  .error {
-    border: 2px solid #f50000;
-    color: #f50000;
-  }
-  </style>
-  
+  <div class="flex flex-column w-100">
+    <span class="pb-5 f-s-12 f-w-500" :class="{ 'color-danger': errorMessage }">
+      {{ label }}
+    </span>
+    <textarea
+      :style="{
+        height: `${height}`,
+        width: `${width}`,
+      }"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :type="type"
+      :class="{
+        error: errorMessage,
+        disabled: disabled,
+      }"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      class="mt-2"
+    ></textarea>
+    <span v-if="errorMessage" class="color-danger f-s-12 f-w-500 pt-5">
+      {{ errorMessage }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { CoreValidations } from "@/composables/CoreValidations";
+
+const errorMessage = ref("");
+const emit = defineEmits(["update:access"]);
+
+const access = defineModel("access");
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  label: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  type: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  modelValue: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  placeholder: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  height: {
+    type: String,
+    default: "40px",
+  },
+  width: {
+    type: String,
+    default: "",
+  },
+  validate: {
+    type: Boolean,
+    default: false,
+  },
+  rules: {
+    type: String,
+    default: "",
+  },
+  minLength: {
+    type: Number,
+    default: 0,
+  },
+  maxLength: {
+    type: Number,
+    default: 0,
+  },
+});
+
+watch(
+  () => props.modelValue,
+  (nVal, oval) => {
+    if (props.validate) {
+      switch (props.rules) {
+        case "email":
+          if (props.modelValue != "") {
+            if (CoreValidations.validEmail(props.modelValue)) {
+              access.value = true;
+              errorMessage.value = "";
+            } else {
+              access.value = false;
+              errorMessage.value = "email not valid";
+            }
+          } else {
+            errorMessage.value = "";
+          }
+          break;
+
+        case "required":
+          if (CoreValidations.validEmpty(props.modelValue)) {
+            access.value = true;
+            errorMessage.value = "";
+          } else {
+            access.value = false;
+            errorMessage.value = "Field is required";
+          }
+          break;
+
+        case "length":
+          const result = CoreValidations.validLength(
+            props.modelValue,
+            props.minLength,
+            props.maxLength
+          );
+          if (result.isValid) {
+            access.value = true;
+            errorMessage.value = result.message;
+          } else {
+            access.value = false;
+            errorMessage.value = result.message;
+          }
+          break;
+      }
+    }
+  },
+  { deep: true }
+);
+</script>
+
+<style scoped lang="scss">
+input {
+  padding: 0 7px;
+  outline: none;
+  border: 1px solid #8481812f;
+  border-radius: 6px;
+  color: black;
+}
+
+.disabled {
+  cursor: not-allowed;
+}
+
+.error {
+  border: 2px solid #f50000;
+  color: #f50000;
+}
+</style>
--- pages/docs/textarea/index.vue
+++ pages/docs/textarea/index.vue
@@ -1,64 +1,188 @@
 <template>
   <div class="flex flex-column">
     <BaseBreadCrumbs />
-    <span class="f-s-14 f-w-600 pt-20 slid-up-animation-3"
-        >Base Textarea</span>
+    <span class="f-s-14 f-w-600 pt-20 slid-up-animation-3">Base Textarea</span>
     <span class="f-s-12 f-w-600 pt-20 slid-up-animation-3"
       >Normal Textarea Style</span
     >
     <div class="flex flex-wrap w-100 mt-15 slid-up-animation-3">
-      <BaseTextarea label="Bio" placeholder="Enter Bio" class="mt-5" />
+      <BaseTextarea
+        label="Username"
+        placeholder="Enter Username"
+        class="mt-5"
+      />
     </div>
-    <CodeRender class="mt-15 slid-up-animation-3" sourceCode='<BaseTextarea label="Bio" placeholder="Enter Bio" />'/>
+    <CodeRender
+      class="mt-15 slid-up-animation-3"
+      sourceCode='<BaseTextarea label="Username" placeholder="Enter Username" />'
+    />
     <span class="f-s-12 f-w-600 pt-20 slid-up-animation-3"
       >Disabled Textarea Style</span
     >
     <div class="flex flex-wrap w-100 mt-15 slid-up-animation-3">
-      <BaseTextarea label="Bio" placeholder="Enter Bio" disabled="true" class="mt-5" />
+      <BaseTextarea
+        label="Username"
+        placeholder="Enter Username"
+        disabled="true"
+        class="mt-5"
+      />
     </div>
-    <CodeRender class="mt-15 slid-up-animation-3" sourceCode='<BaseTextarea label="Bio" placeholder="Enter Bio" disabled="true" />'/>
+    <CodeRender
+      class="mt-15 slid-up-animation-3"
+      sourceCode='<BaseTextarea label="Username" placeholder="Enter Username" disabled="true" />'
+    />
     <span class="f-s-12 f-w-600 pt-20 slid-up-animation-3"
       >Handle Error Just With Bind :errorMessage Props To BaseTextarea</span
     >
     <div class="flex flex-wrap w-100 mt-15 slid-up-animation-3">
-      <BaseTextarea label="Bio" placeholder="Enter Bio" errorMessage="Bio Is Required" class="mt-5" />
+      <BaseTextarea
+        label="Username"
+        placeholder="Enter Username"
+        errorMessage="Username Is Required"
+        class="mt-5"
+      />
     </div>
-    <CodeRender class="mt-15 slid-up-animation-3" sourceCode='<BaseTextarea label="Bio" placeholder="Enter Bio"/>'/>
+    <CodeRender
+      class="mt-15 slid-up-animation-3"
+      sourceCode='<BaseTextarea label="Username" placeholder="Enter Username"/>'
+    />
     <span class="f-s-12 f-w-600 pt-20 slid-up-animation-3"
       >ModelValue Props BaseTextarea</span
     >
-    <BaseTextarea label="Bio" placeholder="Enter Bio" v-model="bio" class="mt-5" />
-    <span class="mt-15 f-s-14 f-w-600">Bio Is {{ bio }}</span>
-    <CodeRender class="mt-15 slid-up-animation-3" sourceCode='<BaseTextarea label="Bio" placeholder="Enter Bio" v-model="bio"/> 
-<span class="mt-15 f-s-14 f-w-600">Bio Is {{ bio }}</span>
+    <div class="flex flex-wrap w-100 mt-15 slid-up-animation-3">
+      <BaseTextarea
+        label="Username"
+        placeholder="Enter Username"
+        v-model="username"
+        class="mt-5"
+      />
+      <span class="mt-15 f-s-14 f-w-600">Username Is {{ username }}</span>
+    </div>
+    <CodeRender
+      class="mt-15 slid-up-animation-3"
+      sourceCode='<BaseTextarea label="Username" placeholder="Enter Username" v-model="username"/> 
+<span class="mt-15 f-s-14 f-w-600">Username Is {{ username }}</span>
 
 <script setup>
-const bio = ref(null)
-</script>'/>
-    <span class="f-s-16 f-w-600 pt-20 slid-up-animation-3">Read For More Components ?</span>
-<div class="flex flex-wrap mt-20">
-  <BaseCard @click="navigateTo('/docs/button')"  class="flex py-10 px-20 align-center cursor-pointer mt-10">
-    <BaseIcon name="mdi:hand-back-right" color="#fff" />
-    <span class="f-s-14 f-w-600 px-2">Button</span>
-  </BaseCard>
-  <BaseCard @click="navigateTo('/docs/input')"  class="flex py-10 px-20 align-center cursor-pointer mt-10 mx-10">
-    <BaseIcon name="mdi:hand-back-right" color="#fff" />
-    <span class="f-s-14 f-w-600 px-2">Input</span>
-  </BaseCard>
-  <BaseCard @click="navigateTo('/docs/avatar')"  class="flex py-10 px-20 align-center cursor-pointer mt-10">
-    <BaseIcon name="mdi:hand-back-right" color="#fff" />
-    <span class="f-s-14 f-w-600 px-2">Avatar</span>
-  </BaseCard>
-  <BaseCard  @click="navigateTo('/docs/select')" class="flex py-10 px-20 align-center cursor-pointer mt-10 mx-10">
-    <BaseIcon name="mdi:hand-back-right" color="#fff" />
-    <span class="f-s-14 f-w-600 px-2">Select Items</span>
-  </BaseCard>
-</div>
+const username = ref(null)
+</script>'
+    />
+    <span class="f-s-12 f-w-600 pt-20 slid-up-animation-3"
+      >Custom Width And Height Textarea Width Props</span
+    >
+    <div class="flex flex-wrap w-100 mt-15 slid-up-animation-3">
+      <BaseTextarea
+        width="300px"
+        height="45px"
+        label="Username"
+        placeholder="Enter Username"
+        class="mt-5"
+      />
+    </div>
+    <CodeRender
+      class="mt-15 slid-up-animation-3"
+      sourceCode='<BaseTextarea width="300px" height="45px" label="Username" placeholder="Enter Username" />'
+    />
+    <span class="f-s-12 f-w-600 pt-20 slid-up-animation-3"
+      >Handle Validation With BaseTextarea</span
+    >
+    <div class="flex flex-column mt-20">
+      <BaseTextarea
+        label="title"
+        placeholder="Enter title"
+        v-model="form.title"
+        validate="true"
+        v-model:access="access"
+        rules="length"
+        minLength="20"
+        maxLength="300"
+      />
+      <BaseTextarea
+        label="bio"
+        placeholder="Enter bio"
+        v-model="form.bio"
+        validate="true"
+        v-model:access="access"
+        rules="length"
+        minLength="3"
+        maxLength="20"
+      />
+      <BaseButton class="mt-10" name="clicked" @click="cliked" />
+    </div>
+    <CodeRender
+      class="mt-15 slid-up-animation-3"
+      sourceCode='<BaseTextarea label="title" placeholder="Enter title" v-model="form.title" validate="true" v-model:access="access"   rules="length" minLength="20" maxLength="300" />
+<BaseTextarea label="bio" placeholder="Enter bio" v-model="form.bio" validate="true" v-model:access="access" rules="length" minLength="3" maxLength="20" />
+<BaseButton class="mt-10" name="clicked" @click="cliked" />
+
+<script setup>
+const access = ref(true)
+const form = ref({
+  title: "",
+  bio: ""
+})
+
+const cliked = () => {
+  if(form.value.title && form.value.bio) {
+   access.value ? alert("valid") : alert("not valid")
+  } else {
+    alert("not valid")
+  }
+}
+</script>'
+    />
+    <span class="f-s-16 f-w-600 pt-20 slid-up-animation-3"
+      >Read For More Components ?</span
+    >
+    <div class="flex flex-wrap mt-20">
+      <BaseCard
+        @click="navigateTo('/docs/button')"
+        class="flex py-10 px-20 align-center cursor-pointer mt-10"
+      >
+        <BaseIcon name="mdi:hand-back-right" color="#fff" />
+        <span class="f-s-14 f-w-600 px-2">Button</span>
+      </BaseCard>
+      <BaseCard
+        @click="navigateTo('/docs/textarea')"
+        class="flex py-10 px-20 align-center cursor-pointer mt-10 mx-10"
+      >
+        <BaseIcon name="mdi:hand-back-right" color="#fff" />
+        <span class="f-s-14 f-w-600 px-2">Text Area</span>
+      </BaseCard>
+      <BaseCard
+        @click="navigateTo('/docs/avatar')"
+        class="flex py-10 px-20 align-center cursor-pointer mt-10"
+      >
+        <BaseIcon name="mdi:hand-back-right" color="#fff" />
+        <span class="f-s-14 f-w-600 px-2">Avatar</span>
+      </BaseCard>
+      <BaseCard
+        @click="navigateTo('/docs/select')"
+        class="flex py-10 px-20 align-center cursor-pointer mt-10 mx-10"
+      >
+        <BaseIcon name="mdi:hand-back-right" color="#fff" />
+        <span class="f-s-14 f-w-600 px-2">Select Items</span>
+      </BaseCard>
+    </div>
   </div>
 </template>
 
 <script setup>
-const bio = ref(null)
+const username = ref("");
+const access = ref(true);
+const form = ref({
+  title: "",
+  bio: "",
+});
+
+const cliked = () => {
+  if (form.value.title && form.value.bio) {
+    access.value ? alert("valid") : alert("not valid");
+  } else {
+    alert("not valid");
+  }
+};
+
 definePageMeta({
   layout: "docs",
 });
